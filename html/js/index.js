cad41cf15b35e13d155ae9f15a3a95d1f2a3bc76
Your Name <you@example.com>
2023-08-17 12:02:35 +0530

    FD

--- html/js/index.js
+++ html/js/index.js
@@ -40,9 +40,9 @@ for (i = 0; i < acc.length; i++) {
     });
 }
 
-const hamburger = document.querySelector(".hamburger-menu");
-const navLinks = document.querySelector(".nav-links");
+// const hamburger = document.querySelector(".hamburger-menu");
+// const navLinks = document.querySelector(".nav-links");
 
-hamburger.addEventListener("click", function () {
-    navLinks.classList.toggle("show");
-});
+// hamburger.addEventListener("click", function () {
+//     navLinks.classList.toggle("show");
+// });
